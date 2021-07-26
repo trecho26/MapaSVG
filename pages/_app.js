d5fde4d800ec3b0c590adf7e505ac41f8bcd753a
trecho26 <hectorlara260698@gmail.com>
2021-07-26 13:40:00 -0600

    Add meta tags for test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mapa interactivo de cobertura. Made with ❤ by 99minutos</title>
+        <meta
+          name="Description"
+          CONTENT="Mapa interactivo de la cobertura nacional de 99 minutos. Author: Trecho26"
+        />
+        <meta
+          name="keywords"
+          content="mapa, mapa interactivo, cobertura, mapa de cobertura, mapa cobertura 99 minutos, 99 minutos, trecho26, mapa nacional de cobertura"
+        />
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
