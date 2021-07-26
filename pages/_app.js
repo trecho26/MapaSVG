88b9907567abc16a571e0ecfa72bb7ce91661e31
trecho26 <hectorlara260698@gmail.com>
2021-07-26 14:52:59 -0600

    Add google meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,15 @@ function MyApp({ Component, pageProps }) {
         <title>Mapa interactivo de cobertura. Made with ❤ by 99minutos</title>
         <meta
           name="Description"
-          CONTENT="Mapa interactivo de la cobertura nacional de 99 minutos. Author: Trecho26"
+          content="Mapa interactivo de la cobertura nacional de 99 minutos en México. Author: Trecho26"
         />
         <meta
           name="keywords"
           content="mapa, mapa interactivo, cobertura, mapa de cobertura, mapa cobertura 99 minutos, 99 minutos, trecho26, mapa nacional de cobertura"
+        />
+        <meta
+          name="google-site-verification"
+          content="hz9audIU5xHEJjlEPLCxseiqJ3MyCj_yx704PJr0yCU"
         />
       </Head>
       <Component {...pageProps} />
